016db8e83c7b2373138a627ccbc81f37621ef129
mohamed.louragini <mohamed.louragini@billcom-consulting.com>
2024-08-02 19:24:24 +0100

    setup project & intialized header component

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      to be implemented by mohamed louragini/ med nour hamzaoui
+    </div>
+  );
+}
